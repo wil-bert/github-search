24694aa61521de6b6581af78329eb3cbc8206613
wil-bert <ianwilbuts@gmail.com>
2020-08-30 23:17:18 +0300

    edit routing.ts

--- src/app/routing.ts
+++ src/app/routing.ts
@@ -1,2 +1,26 @@
-export class Routing {
-}
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { RepoComponent } from './repo/repo.component';
+import { ComponentsComponent } from './components/components.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes:Routes =[
+  {path:"components",component:ComponentsComponent},
+  {path:"profile",component:ProfileComponent},
+  {path:"repo",component:RepoComponent},
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
+  declarations: []
+})
+export class Routing { }
