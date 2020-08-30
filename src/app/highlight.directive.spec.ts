3253655208bf1f1f6c9c3a61b5152592c9705f59
wil-bert <ianwilbuts@gmail.com>
2020-08-30 23:54:45 +0300

    Add highlight directive

---
+++ src/app/highlight.directive.spec.ts
@@ -0,0 +1,7 @@
+import { Highlight.Directive } from './highlight.directive';
+
+describe('Highlight.Directive', () => {
+  it('should create an instance', () => {
+    expect(new Highlight.Directive()).toBeTruthy();
+  });
+});
