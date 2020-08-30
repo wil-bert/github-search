d1404a788a45a18bc2554db214953cf473a4edb9
wil-bert <ianwilbuts@gmail.com>
2020-08-30 12:52:13 +0300

    Edit services

--- src/app/github-search-s/github-search.service.ts
+++ src/app/github-search-s/github-search.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Profile } from './profile-class/profile';
+import { Repo } from './repo';
 
 
 @Injectable({
@@ -8,13 +10,77 @@ import { environment } from '../../environments/environment';
 })
 export class GithubSearchService {
 
+  private username:string;
+  profile: Profile;
+  repo: Repo;
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.profile = new Profile("","","","","",0,0,0,new Date());
+    this.repo = new Repo("","","");
+    this.username = '';
+   }
+   profileRequest(){
+     interface ApiResponse{
+       avatar_url:string;
+       name:string;
+       url:string;
+       bio:string;
+       location:string;
+       public_repos:string;
+       followers:number;
+       following:number;
+       created_on:Date
+     }
 
-  public getProfile(searchQuery){
-    let dataURL = 'https://api.github.com/users/'
+     let promise = new Promise((resolve,reject)=>{
+       this.http.get<ApiResponse>(environment.apiURL+this.username+environment.apiKey).toPromise().then(response=>{
+        this.profile.avatar_url=response.avatar_url
+        this.profile.name=response.name
+        this.profile.url=response.url
+        this.profile.bio=response.bio
+        this.profile.location=response.location
+        this.profile.public_repos=response.public_repos
+        this.profile.followers=response.followers
+        this.profile.following=response.following
+        this.profile.created_at=response.created_on
+
+       resolve()
+       },
+       error =>{
+         this.profile.name = "-Error - Cannot get user"
+         reject(error)
+       }
+    )
+  })
+      return promise
+     }
+
+repoRequest(){
+  interface ApiResponse{
+    name:string;
+    description:string;
+    html_url:string;
+
   }
 
+  let promise = new Promise((resolve,reject)=>{
+    this.http.get<ApiResponse>(environment.apiURL+this.username + environment.apiRepos).toPromise().then(response=>{
+      this.repo.name=response.name
+      this.repo.description=response.description
+      this.repo.html_url=response.html_url
 
+      resolve()
+    },
+    error =>{
+      this.repo.name = "Error - Cannot get Repo"
+      reject(error)
+    }
+  )
+})
+    return promise
 
+  }
+  updateProfile(username:string){
+    this.username = username;
+  }
 }
