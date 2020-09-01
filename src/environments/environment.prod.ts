fdd91c935c51cd7eb495251e693b2d98d0058d3a
wil-bert <ianwilbuts@gmail.com>
2020-09-01 07:42:02 +0300

    Last changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL:"https://api.github.com/users/",
+  apiRepos:"/repos?access_token=ac2afea2f8513076457906a231b42330aa0dc0a4",
+  apiKey:"?access_token=ac2afea2f8513076457906a231b42330aa0dc0a4"
 };
